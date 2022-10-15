817540321c02c6e729b2f2fcad29cff5c250c95c
AZIZ <a.matyaqubov1471@gmail.com>
2022-10-15 14:14:48 +0200

    make navbar mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo1.png'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -20,21 +23,24 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger menu */}
-      <div className="md:hidden">
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile menu  */}
-      <ul className="hidden">
-        <li>
-          <FaTimes />
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Experience</li>
-          <li>Skills</li>
-          <li>Contact</li>
-        </li>
+      <ul
+        className={
+          !nav
+            ? 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'hidden'
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Experience</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/* Social icons */}
