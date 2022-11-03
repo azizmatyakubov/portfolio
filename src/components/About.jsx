956b354b502bdc6b14100a7a764e51ae79bd746b
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-03 20:30:00 +0100

    update about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div>
             <p className="sm:text-right text-4xl font-bold ">
-              Hi, I'm Client, nice to meet you. Please take a look around
+              Hi, I'm Aziz, nice to meet you. Please take a look around
             </p>
           </div>
           <div>
