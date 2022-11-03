c204dab4a202a1e1e83d33546fc0ab4300739d97
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-03 19:18:11 +0100

    complete projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Project from './Project'
 
 const Projects = () => {
   let img =
@@ -14,32 +15,12 @@ const Projects = () => {
           <p className="py-6">// Check out some of my recent projects</p>
         </div>
 
+        {/* Container  */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div
-            style={{
-              backgroundImage: `url(${img})`,
-            }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects  */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Project
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          {/* Project Item */}
+          <Project />
+          <Project />
+          <Project />
         </div>
       </div>
     </div>
