afcfd57851f2bc3141b065fe2438f7c86446ac04
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-03 16:56:51 +0100

    add hover effect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,14 +22,20 @@ const Projects = () => {
         >
           <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
             {/* Hover Effects  */}
-            <div>
-              <span></span>
-              <div>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Project
+              </span>
+              <div className="pt-8 text-center">
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
                 </a>
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
