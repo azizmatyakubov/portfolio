0a59509176d4523f16dd6ad2a94253edf6a81b32
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-03 20:26:14 +0100

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
     <div name="Home" className="w-full h-screen bg-[#0a192f]">
       {/* Container  */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, my name is</p>
+        <p className="text-2xl sm:text-5xl text-pink-600 sm:pb-2 font-bold">
+          Hi, my name is
+        </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Aziz</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Full Stack Developer
@@ -17,12 +20,14 @@ const Home = () => {
           building responsive full-stack web applications.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-2" />
-            </span>
-          </button>
+          <Link to="Projects" smooth={true} offset={50} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-2" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
